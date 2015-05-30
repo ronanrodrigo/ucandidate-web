34010e59ccc9ece6802f608f742b7eaebed1e0d4
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-05-30 17:12:22 -0300

    Pristine

--- javascripts/ucandidate-web.js
+++ javascripts/ucandidate-web.js
@@ -1,25 +1,28 @@
 angular.module('ucandidate-web', [])
-  .controller("CandidateCtrl", ['$http', function($http){
+  .controller("CandidateCtrl", ['$http', '$scope', function($http, $scope){
     var self = this;
-    var nanobar = new Nanobar({bg: "#337ab7"})
+    var nanobar = new Nanobar({bg: "#337ab7"});
 
     self.startData = function(){
       self.candidate = {};
       self.candidate.knowledges_attributes = [{item: "HTML", level: null}, {item: "CSS", level: null}, {item: "JavaScript", level: null}, {item: "Python", level: null}, {item: "Django", level: null}, {item: "Desenvolvimento iOS", level: null}, {item: "Desenvolvimento Android", level: null}];
-    }
+    };
 
-    self.startData()
+    self.startData();
 
     self.submit = function () {
-      console.log(self.candidateForm)
-      nanobar.go(10)
+      nanobar.go(10);
       $http.post("http://ucandidate-api.herokuapp.com/api/candidates", {"candidate" : self.candidate}).then(function(response){
-        nanobar.go(100)
-        self.startData()
-        swal("Informaçoes recebidas!", "Em breve você receberá um e-mail sobre nossas vagas.", "success")
+        nanobar.go(100);
+        self.startData();
+        $scope.$watch('candidateForm', function(candidateForm) {
+          candidateForm.$setPristine();
+          candidateForm.$setUntouched();
+        });
+        swal("Informaçoes recebidas!", "Em breve você receberá um e-mail sobre nossas vagas.", "success");
       }, function(errResponse){
-        nanobar.go(100)
-        console.log("Erro ao comunicar com a API")
+        nanobar.go(100);
+        console.log("Erro ao comunicar com a API");
       })
     }
   }])
